b2b6ff3ff0787dbbacf43bc65908df652b574ad9
dodomuk <siderid@naver.com>
2021-07-29 18:05:17 +0900

    Create JpatravelclubApplication.java

---
+++ back/jpatravelclub/src/main/java/com/example/jpatravelclub/JpatravelclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpatravelclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatravelclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpatravelclubApplication.class, args);
+    }
+
+}
